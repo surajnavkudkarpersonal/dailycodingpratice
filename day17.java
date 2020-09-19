52ca39da140045526b0220b4f4e0cbb0a2f1c7b8
surajnavkudkar <suraj@ridecell.com>
2020-09-19 16:15:30 +0530

    day17

--- day17.java
+++ day17.java
@@ -28,3 +28,27 @@ class Day17 {
   
         return dp[m][n]; 
     }
+
+    public int minPathSumSingleloop(int[][] grid) {
+        int m = grid.length-1;
+        int n = grid[0].length-1;
+ 
+        int dp[][]=new int[m+1][n+1];
+  
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] +=grid[i][j];
+                if(i>0 && j>0) {
+                    dp[i][j]+=Math.min(dp[i-1][j], dp[i][j-1]) ;
+                } else if(i>0) {
+                    dp[i][j]+=dp[i-1][j];
+                } else if(j>0) {
+                    dp[i][j]+=dp[i][j-1];
+                }  
+            }
+        }
+  
+        return dp[m][n]; 
+    }
+
+}
